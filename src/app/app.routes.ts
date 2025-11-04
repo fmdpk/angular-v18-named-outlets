ffec127e22ffb5a14a989153b09752c262797e3a
farzin mossaed <fmossaed@gmail.com>
2025-11-04 15:47:31 +0330

    work on project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,7 @@ export const routes: Routes = [
   //     { path: 'item/:id', component: ItemDetailComponent, outlet: 'detail' } // if the outlet is inside HomeComponent
   //   ]
   // },
-  { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
-    // children: [
-    //   { path: 'item/:id', component: ItemDetailComponent, outlet: 'detail' } // if the outlet is inside HomeComponent
-    // ]
-  },                     // primary outlet
+  { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),},  // primary outlet
   { path: 'item/:id', loadComponent: () => import('./item-detail/item-detail.component').then(m => m.ItemDetailComponent), outlet: 'detail' },
   { path: 'sidebar', loadComponent: () => import('./sidebar/sidebar.component').then(m => m.SidebarComponent), outlet: 'sidebar' }
 ];
