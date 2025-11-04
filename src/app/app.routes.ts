4492fe195406278250f870f8aebf4fbcf3fc5dea
farzin mossaed <fmossaed@gmail.com>
2025-11-04 15:48:43 +0330

    work on project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {ItemDetailComponent} from './item-detail/item-detail.component';
-import {SidebarComponent} from './sidebar/sidebar.component';
 
 export const routes: Routes = [
   // { path: '', component: HomeComponent,
   //   children: [
-  //     { path: 'item/:id', component: ItemDetailComponent, outlet: 'detail' } // if the outlet is inside HomeComponent
+  //     { path: 'item/:id', loadComponent: () => import('./item-detail/item-detail.component').then(m => m.ItemDetailComponent), outlet: 'detail' } // if the outlet is inside HomeComponent
   //   ]
   // },
   { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),},  // primary outlet
